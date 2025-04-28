b71c97403637811cfd31a7c26ee54676e06ff5c7
Martin Jönsson <martin.t.joensson@gmail.com>
2025-04-28 02:25:45 +0200

    Added more assets to Catastrophic Failure section

--- src/components/CatastrophicFailure/Catastrophic.jsx
+++ src/components/CatastrophicFailure/Catastrophic.jsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Rules } from "./Rules";
-import catastrophic from "../../assets/catastrophic.png"
+import catastrophic from "../../assets/catastrophic.png";
+import frog from "../../assets/FrogMerchantbig.jpg";
+import chest from "../../assets/Chest.png";
+import coin from "../../assets/Coin.png";
 
 export const Catastrophic = () => {
   return (
-    <div className="wide:w-2/3 wide:m-auto bg-white text-black flex flex-col ">
+    <div className="wide:w-2/3  bg-white text-black flex flex-col">
       <img src={catastrophic} className="m-auto pb-4" />
-      <h2  className="py-4 hidden">Catastrophic Failure</h2>
-      <p className="w-1/2 m-auto"> Catastrophic Failure is an upcoming RPG by me and my friends. The basic premise being that you get to do pretty much everything, but the consequences might be dire.</p>
-      <p>Check back soon for more!</p>
+      <h2 className="py-4 hidden">Catastrophic Failure</h2>
+      <img
+        src={chest}
+        className="w-1/5 m-auto rounded-full"
+        alt="A pixelart chest."
+      />
+
+      <p className="w-1/2 m-auto">
+        {" "}
+        Catastrophic Failure is an upcoming RPG by me and my friends. The basic
+        premise being that you get to do pretty much everything, but the
+        consequences might be dire.
+      </p>
+
+      <p className="w-1/2 m-auto">Check back soon for more!</p>
+      <img
+        src={coin}
+        className="w-1/5 m-auto rounded-full"
+        alt="A pixelart frog with blue sky."
+      />
       {/* <Rules /> */}
     </div>
   );
