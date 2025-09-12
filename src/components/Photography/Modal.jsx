76c7931a7d416a6a3aba2f2a46c755e2904f51af
Martin Jönsson <martin.t.joensson@gmail.com>
2025-09-12 16:49:03 +0200

    Changed to new Tailwind syntax

--- src/components/Photography/Modal.jsx
+++ src/components/Photography/Modal.jsx
@@ -10,7 +10,7 @@ export const Modal = ({ image, alttext, title, visible, setVisible }) => {
       {visible ? (
         <div
           onClick={handleClick}
-          className="bg-gray-950  backdrop-blur-sm bg-opacity-80 fixed flex min-h-full items-center w-full left-0 top-0"
+          className="bg-gray-950/80  backdrop-blur-sm fixed flex min-h-full items-center w-full left-0 top-0"
         >
           <div className="flex flex-col p-8 h-screen mx-auto place-content-center">
             <span className="text-3xl cursor-pointer text-right">&times;</span>
